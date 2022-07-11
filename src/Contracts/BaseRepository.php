6ec1675906abd617fe597f1dcfc0e1066c190732
Khris <contact@devkhris.com>
2022-07-11 03:07:41 -0500

    fix: fixed namespace for interface

--- src/Contracts/BaseRepository.php
+++ src/Contracts/BaseRepository.php
@@ -4,7 +4,7 @@ namespace DevKhris\LaraArchRepository\Contracts;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
-use DevKhris\LaravelRepository\Interfaces\EloquentRepositoryInterface;
+use DevKhris\LaraArchRepository\Interfaces\EloquentRepositoryInterface;
 
 class BaseRepository implements EloquentRepositoryInterface
 {
@@ -25,61 +25,61 @@ class BaseRepository implements EloquentRepositoryInterface
         $this->model = $model;
     }
 
-	public function all(array $columns = ['*'], array $relations = []): Collection
-	{
-		return $this->model->with($relations)->get($columns);
-	}
+    public function all(array $columns = ['*'], array $relations = []): Collection
+    {
+        return $this->model->with($relations)->get($columns);
+    }
 
-	public function allDeleted(): Collection
-	{
-		return $this->model->onlyTrashed()->get();
-	}
+    public function allDeleted(): Collection
+    {
+        return $this->model->onlyTrashed()->get();
+    }
 
-	public function getById(
-		int $id,
-		array $columns = ['*'],
-		array $relations = [],
-		array $data = []
-	): Model {
-		return $this->model->select($columns)->with($relations)->findOrFail($id)->append($data);
-	}
+    public function getById(
+        int $id,
+        array $columns = ['*'],
+        array $relations = [],
+        array $data = []
+    ): Model {
+        return $this->model->select($columns)->with($relations)->findOrFail($id)->append($data);
+    }
 
-	public function getDeletedById(int $id): Model
-	{
-		return $this->model->withTrashed()->findOrFail($id);
-	}
+    public function getDeletedById(int $id): Model
+    {
+        return $this->model->withTrashed()->findOrFail($id);
+    }
 
-	public function getOnlyDeletedById(int $id): Model
-	{
-		return $this->model->onlyTrashed()->findOrFail($id);
-	}
+    public function getOnlyDeletedById(int $id): Model
+    {
+        return $this->model->onlyTrashed()->findOrFail($id);
+    }
 
-	public function store(array $payload = []): Model
-	{
-		$model = $this->model->create($payload);
+    public function store(array $payload = []): Model
+    {
+        $model = $this->model->create($payload);
 
-		return $model->fresh();
-	}
+        return $model->fresh();
+    }
 
-	public function update(int $id, array $payload = []): Model
-	{
-		$model = $this->getById($id);
+    public function update(int $id, array $payload = []): Model
+    {
+        $model = $this->getById($id);
 
-		return $model->update($payload);
-	}
+        return $model->update($payload);
+    }
 
-	public function restoreById(int $id): bool
-	{
-		return $this->getOnlyDeletedById($id)->restore();
-	}
+    public function restoreById(int $id): bool
+    {
+        return $this->getOnlyDeletedById($id)->restore();
+    }
 
-	public function deleteById(int $id): bool
-	{
-		return $this->getById($id)->delete();
-	}
+    public function deleteById(int $id): bool
+    {
+        return $this->getById($id)->delete();
+    }
 
-	public function permanentlyDeleteById(int $id): bool
-	{
-		return $this->getDeletedById($id)->forceDelete();
-	}
+    public function permanentlyDeleteById(int $id): bool
+    {
+        return $this->getDeletedById($id)->forceDelete();
+    }
 }
